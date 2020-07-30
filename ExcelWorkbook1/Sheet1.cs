bfaf8c3a42d572fc55abbef42ec2c1523e6e04c7
Takahiro Hanada <43534055+takahiro-hanada@users.noreply.github.com>
2020-07-31 08:19:48 +0900

    Add files.

---
+++ ExcelWorkbook1/Sheet1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using Microsoft.VisualStudio.Tools.Applications.Runtime;
+using Excel = Microsoft.Office.Interop.Excel;
+using Office = Microsoft.Office.Core;
+
+namespace ExcelWorkbook1
+{
+    public partial class Sheet1
+    {
+        private void Sheet1_Startup(object sender, System.EventArgs e)
+        {
+        }
+
+        private void Sheet1_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        #region VSTO デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(Sheet1_Startup);
+            this.Shutdown += new System.EventHandler(Sheet1_Shutdown);
+        }
+
+        #endregion
+
+    }
+}
